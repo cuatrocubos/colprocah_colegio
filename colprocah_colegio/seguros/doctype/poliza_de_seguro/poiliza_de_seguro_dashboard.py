33526be4dab363e98d00006cf9c92dfc8cdaf16c
Jorge Girón <jorge.giron.29@gmail.com>
2025-01-21 22:05:42 -0600

    feat: seguros

---
+++ colprocah_colegio/seguros/doctype/poliza_de_seguro/poiliza_de_seguro_dashboard.py
@@ -0,0 +1,9 @@
+from frappe import _
+
+def get_date():
+  return {
+    "fieldname": "poliza_de_seguro"
+  },
+  "transactions": [
+    {"label": _("Reference"), "items": ["Planes de Seguro"]}
+  ]
